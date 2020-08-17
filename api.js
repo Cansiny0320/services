4e1f12b95ed2652b47d6fc6dc9afcd986c68963a
Cansiny0320 <1136843910@qq.com>
2020-08-17 17:51:17 +0800

    feat: init

---
+++ api.js
@@ -0,0 +1,14 @@
+/**
+ * 所有的接口请求
+ */
+
+import { get } from './http';
+
+export const hotQuestion = () => get('/hot');
+
+/**
+ *
+ * @param {*} id 用户id
+ */
+
+export const userInfo = id => get('/user', { id });
